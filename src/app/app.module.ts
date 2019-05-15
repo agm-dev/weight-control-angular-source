03e885134fa6ef5702187241b89327f94513ec99
Adrian Gonzalo <gonzahh@gmail.com>
2019-05-15 21:04:52 +0200

    added plotyly to graph component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AddDataComponent } from './add-data/add-data.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule } from 'angular-plotly.js'
+PlotlyModule.plotlyjs = PlotlyJS;
 
 import { LoggerService } from './logger.service';
 import { StateService } from './state.service';
+
+import { AppComponent } from './app.component';
+import { AddDataComponent } from './add-data/add-data.component';
 import { GraphComponent } from './graph/graph.component';
 import { SettingsComponent } from './settings/settings.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -23,6 +28,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    PlotlyModule,
   ],
   providers: [
     LoggerService,
--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -7,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphComponent implements OnInit {
 
-  constructor() { }
+  public graph:any; // TODO: change this to Graph class type
+  public graphStyle:any;
+
+  constructor() {
+    this.graph = {
+      data: [
+        {
+          x: ['01/05', '04/05', '23/05'],
+          y: [78.5, 78.3, 79.4],
+          type: 'scatter',
+          mode: 'linex+points',
+          marker: { color: 'red' },
+        },
+      ],
+      layout: {
+        autosize: true,
+      }
+    }
+
+    this.graphStyle = {
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+    }
+  }
 
   ngOnInit() {
   }
